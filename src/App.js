1a7a16b1023446d215a828de996b16be0ffde770
h150362 <atlefoe@hotmail.com>
2018-11-07 17:17:37 +0100

    i made some changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		    TjoheiOgDuddeliDei <code>src/App.js</code> Preben.	
+		    changes <code>src/App.js</code> changes.	
           </p>
           <a
             className="App-link"
